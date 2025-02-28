35215c0110c6f0b7ac8dfd453c63b68319d3eebb
hemanthgowdas <hemanthgs17@gmail.com>
2025-02-28 06:13:17 +0000

    debug program answer is under class solution

--- leetcode_easy/two_sum(1).py
+++ leetcode_easy/two_sum(1).py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
@@ -6,3 +8,7 @@ class Solution:
                 if sum == target:
                     return[i,j]
         return []     
+    
+sol = Solution()
+result = sol.twoSum([2, 7, 11, 15], 21)
+print("Output:", result)
